55c2696285396601d93642b345ba60c3ab09f12c
aashutosh148 <aashutoshsingh468847@gmail.com>
2024-06-20 22:01:38 +0530

    fixed mode toggle

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -12,9 +12,9 @@ const DarkModeToggle = ({ darkMode, toggleDarkMode }) => {
                     checked={darkMode}
                     onChange={toggleDarkMode}
                 />
-                <div className={`w-16 h-8 rounded-full ${!(darkMode) ? 'bg-slate-700' : 'bg-gray-300'}`}>
+                <div className={`w-16 h-8 rounded-full ${!(darkMode) ? 'bg-slate-700' : 'bg-gray-700'}`}>
                     <span
-                        className={`absolute left-1 top-1 w-6 h-6 rounded-full shadow-md transform transition-transform ${(!darkMode) ? 'translate-x-8 bg-slate-700' : 'bg-slate-500'
+                        className={`absolute left-1 top-1 w-6 h-6 rounded-full shadow-md transform transition-transform ${(!darkMode) ? 'translate-x-8 bg-slate-700' : 'bg-slate-700'
                             }`}
                     >
                         {(!darkMode) ? <img src="./yellow-moon.svg" alt="Moon Logo" className="" />: <img src="/sun-color-icon.svg" alt="Sun Logo" className="" />}
